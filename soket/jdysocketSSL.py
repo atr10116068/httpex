72da09239a6190b7d210c75fe8ae5d1ec1574b64
Topik <dncatr@gmail.com>
2022-08-14 11:57:33 +0700

    asd

--- soket/jdysocketSSL.py
+++ soket/jdysocketSSL.py
@@ -1,15 +1,14 @@
-import websocket
+import websockets
 import json
 import time
 import requests
 import sys
 import seting
-import getlive,sys
+import getlive,sys,random
 import ambil
 from datetime import datetime
 import pytz
 from tinydb import *
-
 
 import asyncio
 import ssl
@@ -23,10 +22,11 @@ ssl_context.load_verify_locations(certifi.where())
 
 db = TinyDB("data.json")
 tbl = Query()
-db.truncate()
+# db.truncate()
 
 tokk = ambil.token()
 persi = seting.versi()
+# token = tokk[int(input("token no : "))]
 token = tokk[int(sys.argv[1])]
 
 room = getlive.roomall()
@@ -41,27 +41,18 @@ for i in room:
 
 # inp = input("room nomor : ")
 # idroom = room[int(inp)-1]["live_id"]
+# print("\nTarget Room : "+room[int(inp)-1]["nickname"])
 print(f"\nTarget Room : {namanya}")
 
 datan = b"ping"
 uriweb = "wss://dt001wsgew.qrdnk.cn/?token="+token
-print(uriweb)
+
 param = {
     "upgrade": "websocket",
     "connection": "Upgrade",
     "Accept-Encoding": "gzip",
     "host": "yoogs01wltb.dt01showxx03.com",
-    "user-agent": "Mozilla/5.0 (Linux; Android 8.1.0; Redmi 5 Plus Build/OPM1.171019.019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.120 Mobile Safari/537.36",
-}
-game = {
-    "baijiale_1": "Ba",
-    "toubao_1": "Si",
-    "kuaisan_1": "Th",
-    "longhu_1": "Dr",
-    "lunpan_1": "Ro",
-    "honglv_1": "Co",
-    "m12_1": "M12",
-    "liuhecai_1": "M6",
+    "user-agent": f"HS-Android Mozilla/5.0 (Linux; Android 8.1.0; Redmi 5 Plus Build/OPM1.17{random.randint(1000,9999)}.019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/98.0.{random.randint(1000,9999)}.82 Mobile Safari/537.36",
 }
 
 datajuday = {
@@ -149,13 +140,11 @@ game = {
 #     if idxg == int(targetgameid):
 #         targetgame = pgp
 #     idxg += 1
-
-
 targetgame=sys.argv[3]
+
 def rp(str):
     bbb = str.replace("\'", "\"")
     return bbb
-
 
 
 datroom={
@@ -164,6 +153,152 @@ datroom={
     "kosong_brp_kali":0,
     "isexit":False
     }
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import translatepy as trs
+dat = {
+    "admin": [
+        "1119943580",
+        "1708242561",
+        "1696182045",
+        "1665699504",
+        "1037219061",
+        "1188860696",
+        "1696985118",
+        "1702716897",
+        "1702616359",
+        "1000361907",
+        "1298487258"
+    ],
+    "minimumlvl": 7
+}
+host=["1"]
+lepel = {
+    "1": 15,  # host
+    "13": 2,
+    "14": 3,
+    "2": 4,
+    "4": 5,
+    "5": 6,
+    "6": 7,
+    "7": 8,
+    "9": 9,
+    "8": 10,
+    "10": 11,
+    "11": 12,
+    "3": 13,
+    "12": 14,  # admin
+}
+
+tokenhost = ambil.tokenhost()
+def gas2(id, tok):
+    uri1 = f"https://wjxwd01mwyo.dt01showxx02.com/App/Live/RealTimePeopleList?live_id={id}&page=1"
+    headers = {
+        "User-Agent": f"HS-Android Mozilla/5.0 (Linux; Android 8.1.0; Redmi 5 Plus Build/OPM1.17{random.randint(1000,9999)}.019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/98.0.{random.randint(1000,9999)}.82 Mobile Safari/537.36",
+        "BundleIdentifier": "anchor",
+        "X-Token": tok,
+        "Accept-Encoding": "identitpython host/host.pyy",
+        "X-Version": persi,
+        "Host": "wjxwd01mwyo.dt01showxx02.com",
+        "Connection": "Keep-Alive",
+    }
+    try:
+        req1 = requests.get(uri1, headers=headers)
+        ress1 = json.loads(req1.text)["result"]["list"]
+        return ress1
+    except:
+        return [9, 9]
+
+def cariviwer(udata):
+    udata["utex"] = udata["utex"].replace("cariakun ", "")
+    viwerny = udata["utex"]
+    roomxx = getlive.roomall()
+    nemu = False
+    for iii in roomxx:
+        idroomss = iii["live_id"]
+        namaroom = iii["nickname"]
+        try:
+            datas = gas2(idroomss, tokenhost)
+            for pp in datas:
+                nama = pp["nickname"]
+                if nama == viwerny:
+                    nemu = {0: nama, 1: namaroom}
+        except:
+            print("Gagal Capture Data")
+    if nemu != False:
+        sen(idroom, token, f"{nemu[0]} di {nemu[1]}")
+    else:
+        sen(idroom, token, f"Ga nemu bang")
+
+def clvl(udata):
+    try:
+        udata["utex"] = udata["utex"].replace("changelvl ", "")
+        dat["minimumlvl"] = int(udata["utex"])
+        sen(idroom, token, f"Sudah di sett ke {udata['utex']}")
+    except Exception as e:
+        print(f"Error : {e}")
+
+gid = []
+imb = ["karena", "ketika", "saat", "dan", "melihat", "mendengar"]
+gamechat = {}
+
+
+def jumhost():
+    rgame = getlive.roomgame(
+        {"idx": 1, "result": [], "rapihkanjson": [], "terfilter": []})
+    rindo = getlive.roomindo(
+        {"idx": 1, "result": [], "rapihkanjson": [], "terfilter": []})
+    rsexy = getlive.roomsexy(
+        {"idx": 1, "result": [], "rapihkanjson": [], "terfilter": []})
+    dat = {
+        "game": len(rgame),
+        "indo": len(rindo),
+        "sexy": len(rsexy),
+    }
+    return dat
+
+def sen(id, tok, tex):
+    uri = "https://wjxwd01mwyo.dt01showxx02.com/App/Live/SendMsg"
+    headers = {
+        "user-agent": f"HS-Android Mozilla/5.0 (Linux; Android 8.1.0; SM-J730F Build/{random.randint(1000,9999)}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.{random.randint(1000,9999)}.129 Mobile Safari/537.36",
+        "bundleidentifier": "user",
+        "x-token": tok,
+        "accept-encoding": "identity",
+        "host": "wjxwd01mwyo.dt01showxx02.com",
+        "connection": "keep-alive"
+    }
+    para = {"live_id": id, "content": tex}
+
+    req = requests.get(uri, params=para, headers=headers)
+    ress = json.loads(req.text)
+    return ress
+
+def trans(udata):
+    try:
+        udata["utex"] = udata["utex"].replace("tr ", "")
+        desx = udata["utex"].split(" ")[0]
+        if desx=="cn":
+            desx="zh-tw"
+        se0 = udata["utex"].split(" ")
+        del se0[0]
+        texxx = ""
+        for tp in se0:
+            texxx += f'{tp} '
+
+        # print(f"texx : {texx}")
+        # print(f"desx : {desx}")
+        # print(f"texxx : {texxx}")
+        hasi = trs.tpy(texxx, desx)
+        # <<<< ['入力しようとしています', 'Nyūryoku shiyou to shite imasu'] >>>>>>
+        sen(idroom, token, hasi[1])
+    except Exception as e:
+        print(f"Error : {e}")
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+import _thread as thread
 def xrespon(message):
     datadadu = json.loads(message)
     # print(datadadu[0])
@@ -172,11 +307,12 @@ def xrespon(message):
             tz = pytz.timezone("Asia/Jakarta")
             now = datetime.now(tz)
             menit=now.strftime("%M")
-            if datroom["viwermasuk"]==0:
-                if datroom["kosong_brp_kali"]<2:
+            if datroom["viwermasuk"]<2:
+                if datroom["kosong_brp_kali"]<10:
                     datroom["kosong_brp_kali"]+=1
                 else:
                     datroom["isexit"]=True
+                    db.remove(where('tokenno') == sys.argv[1])
                     sys.exit()
             else:
                 datroom["kosong_brp_kali"]=0
@@ -197,7 +333,7 @@ def xrespon(message):
             print(f'\t\t{datadadu[0]["data"]["msg_body"]["client_id"]}')
             uriweb = "https://wjxwd01mwyo.dt01showxx02.com/App/LiveEnter/JoinGroup"
             headers = {
-                "User-Agent": "HS-Android Mozilla/5.0 (Linux; Android 8.1.0; Pixel C Build/OPM1.171019.019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36",
+                "User-Agent": f"HS-Android Mozilla/5.0 (Linux; Android 8.1.0; Redmi 5 Plus Build/OPM1.17{random.randint(1000,9999)}.019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/98.0.{random.randint(1000,9999)}.82 Mobile Safari/537.36",
                 "BundleIdentifier": "user",
                 "X-Token": token,
                 "Accept-Encoding": "identity",
@@ -281,8 +417,129 @@ def xrespon(message):
                 print(e)
                 print(datadadu[0]["data"]["msg_body"])
 
-    except:
-        pass
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        if datadadu[0]["action"] == "send_msg":
+            utex = datadadu[0]['data']['msg_body']['content']
+            udata = {
+                "uid": datadadu[0]['data']['msg_body']['show_id'],
+                "ulvl": datadadu[0]['data']['msg_body']['vip'],
+                "uname": datadadu[0]['data']['msg_body']['nickname'],
+                "utex": utex,
+            }
+
+            if udata['utex'].lower() in ["taro", "bangtaro","bang taro"]:
+                bawel = [
+                    "apa sih... cok",
+                    "apaan?",
+                    "oiiiii",
+                    "kenapa?",
+                    "gw disini...",
+                    "ada apa 𓂸",
+                    "ngomong mulu lu, ga aus apa?",
+                ]
+                tex = random.choice(bawel)
+                sen(idroom, token, tex)
+            try:
+                if utex.startswith("."):
+                    texx = utex.replace(".", "")
+                    udata["utex"] = texx
+                    if udata['utex'] == "reset":
+                        if udata["uid"] in dat["admin"] or udata["ulvl"] in host:
+                            gamechat.clear()
+                            gid.clear()
+                        else:
+                            tex = "Hanya adminku yang boleh"
+                            sen(idroom, token, tex)
+                    if udata['utex'] == "cek":
+                        tex = f"{len(gid)} akun ikutan"
+                        sen(idroom, token, tex)
+                    if udata['utex'] == "main":
+                        if udata["uid"] in dat["admin"] or udata["ulvl"] in host:
+                            for tex in ["ketik .add [nama]-[kerja]-[nama]-[kerja]", 'contoh .add budi-terkejut-alex-uncep']:
+                                sen(idroom, token, tex)
+
+                    elif udata['utex'] == "acak":
+                        if udata["uid"] in dat["admin"] or udata["ulvl"] in host:
+                            print(gid)
+                            print(gamechat)
+                            disp = f"{gamechat[random.choice(gid)][0]} {gamechat[random.choice(gid)][1]} {random.choice(imb)} {gamechat[random.choice(gid)][2]} {gamechat[random.choice(gid)][3]}"
+                            sen(idroom, token, disp)
+                        else:
+                            tex = "Hanya adminku yang boleh"
+                            sen(idroom, token, tex)
+
+                    elif udata['utex'].startswith("add "):
+                        dgame = texx.replace("add ", "")
+                        dgames = dgame.split("-")
+                        if len(dgames) == 4:
+                            gamechat[udata["uid"]] = dgames
+                            if udata["uid"] not in gid:
+                                gid.append(udata["uid"])
+                            tex = "Sudah ditambah"
+                            sen(idroom, token, tex)
+                        else:
+                            tex = "contoh nama-ekspresi-nama-ekspresi"
+                            sen(idroom, token, tex)
+                            tex = "contoh budi-terkejut-rudi-tertawa"
+                            sen(idroom, token, tex)
+            except Exception as e:
+                print(e)
+
+            if utex.lower().startswith("taro "):
+                texx = utex.lower().replace("taro ", "")
+                udata["utex"] = texx
+                try:
+                    if lepel[udata["ulvl"]] >= dat["minimumlvl"] or udata["uid"] in dat["admin"]:
+                        if udata['utex'].startswith("tr "):
+                            trans(udata)
+                        elif udata['utex'].startswith("cariakun "):
+                            udata["utex"]=utex.replace("taro ", "")
+                            cariviwer(udata)
+                        elif udata['utex'].startswith("changelvl "):
+                            if udata["uid"] in dat["admin"] or udata["ulvl"] in host:
+                                clvl(udata)
+                            else:
+                                tex = "Hanya adminku yang boleh"
+                                sen(idroom, token, tex)
+
+                        if udata['utex'] == "siapa aku?":
+                            if udata["uid"] in dat["admin"]:
+                                tex = "kamu itu adminku"
+                                sen(idroom, token, tex)
+                            elif udata["ulvl"] in host:
+                                tex = "kamu itu Host"
+                                sen(idroom, token, tex)
+                            else:
+                                tex = "kamu viwer biasa"
+                                sen(idroom, token, tex)
+                        elif udata['utex'] == "bisa apa aja?":
+                            texs = [
+                                "-> taro siapa aku?",
+                                "-> taro cariakun [nama]",
+                                "-> taro tr [bahasa] [text]",
+                                "-> taro jumlah host",
+                            ]
+                            for tex in texs:
+                                sen(idroom, token, tex)
+                                time.sleep(2)
+                        elif udata['utex'] == "jumlah host":
+                            dats = jumhost()
+                            sen(idroom, token, f'{dats["game"]} host game')
+                            sen(idroom, token,
+                                f'{dats["indo"]} host adorable')
+                            sen(idroom, token, f'{dats["sexy"]} host sexy')
+                    else:
+                        tex = f"Hanya dapat di gunakan oleh LVL {str(dat['minimumlvl'])} keatas"
+                        sen(idroom, token, tex)
+                except Exception as e:
+                    print(f"Error : {e}")
+
+            print(
+                f" > [{udata['uid']}][{lepel[udata['ulvl']]}] {udata['uname']}\t: {udata['utex']}")
+    except Exception as e:
+        print(f"Errorr : {e}")
+
 
 while True:
     if datroom["isexit"]==True:
@@ -300,10 +557,3 @@ while True:
         asyncio.get_event_loop().run_until_complete(test())
     except Exception as e:
         print(f"Error : {e}")
-
-
-
-
-
-
-
